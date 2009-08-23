b5f8a1536f915b6e8a416589c732b2a47aa10513
Steven Wilkin <stevenwilkin@gmail.com>
2009-08-23 22:26:32 +0100

    infinte scrolling vertical jcarousel

--- public/js/trends.js
+++ public/js/trends.js
@@ -1,2 +1,24 @@
+function itemVisibleIn(carousel, item, i, state, evt){
+	var idx = carousel.index(i, $('#images ol li').length);
+	var h = $($('#images li')[idx - 1]).html();
+	carousel.add(i, h);
+}
+
+function itemVisibleOut(carousel, item, i, state, evt){
+  carousel.remove(i);
+}
+
 $(document).ready(function(){
+	$('#images ol').hide();
+	$('#images').append('<ul id="carousel" class="jcarousel-skin-tango">');
+	$('#carousel').jcarousel({
+		vertical: true,
+		scroll: 1,
+		auto: 1,
+		buttonNextHTML: null,
+		buttonPrevHTML: null,
+		wrap: 'circular',
+		itemVisibleInCallback: {onBeforeAnimation: itemVisibleIn},
+		itemVisibleOutCallback: {onAfterAnimation: itemVisibleOut}
+	});
 });
